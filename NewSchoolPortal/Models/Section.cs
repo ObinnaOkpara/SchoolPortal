6cb812121f382dc4e8a47c521ee2239542c11a33
Ofoedu Frank Ebuka <Frankofoedu@gmail.com>
2018-09-06 16:08:47 +0100

    Initial commit

---
+++ NewSchoolPortal/Models/Section.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewSchoolPortal.Models
+{
+    public class Section
+    {
+        public int SectionID { get; set; }
+        public int ClassID { get; set; }
+        public string SectionName { get; set; }
+        public string Decription { get; set; }
+        public int StaffID { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ NewSchoolPortal/Models/Staff.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewSchoolPortal.Models
+{
+    public class Staff
+    {
+        public int StaffID { get; set; }
+        public bool isAdmin { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OtherNames { get; set; }
+        public string Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string StateOfOrigin { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Position { get; set; }
+        public string Image { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public virtual ICollection<Section> Sections { get; set; }
+        public virtual ICollection<Subject> Subjects { get; set; }
+
+    }
+}
